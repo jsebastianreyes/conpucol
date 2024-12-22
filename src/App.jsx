bc5b04c9c9a239b737d88c810a4c76695bdcbfad
Juan Sebastián Reyes <40282876+jsebastianreyes@users.noreply.github.com>
2024-12-22 11:32:45 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Loading from "./components/loading";
 import Error from './components/error';
 import ListCategories from './components/list-categories';
 import { Toaster } from 'react-hot-toast';
-import { useState} from "react";
+import { useState, useEffect, useRef } from "react";
 
 
 
@@ -16,18 +16,37 @@ function App() {
   const [month, setMonth] = useState('Seleccionar Mes')
   const [category, setCategory] = useState('Default')
   const {courses, fail, loading} = useFetch()
+  const  observedDiv = useRef();
+  const [height, setHeight] = useState();
 
- 
+  useEffect(()=> {
+    if (!observedDiv.current) {
+      return;
+    }
+
+    const resizeObserver = new ResizeObserver(() => {
+     
+      if(observedDiv.current.offsetHeight !== height) {
+
+        setHeight(observedDiv.current.offsetHeight);
+        window.parent.postMessage(height, "*");
+      }
+    });
+  
+    resizeObserver.observe(observedDiv.current);
+
+    return function cleanup() {
+      resizeObserver.disconnect();
+    }
+  }, [observedDiv.current])
 
 
-
-
+  console.log(height)
  
   return (
 
-    <> 
+    <div ref={observedDiv}> 
     <div><Toaster /></div>
-    {console.log(windowSize)}
     <ListCategories setCategory={setCategory}/>
     <Header setSearch={setSearch} 
     courses={courses.data} 
@@ -38,7 +57,7 @@ function App() {
       {fail &&  <Error/> }
       {loading && <Loading /> }
       {!fail && !loading > 0 && <ListCourses search={search} courses={courses} month={month} city={city} category={category} /> } 
-    </>
+    </div>
   )
 }
 

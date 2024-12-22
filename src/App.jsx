85daa8b40f7ad4300acfcedfb97adf544ea6ef50
Juan Sebastián Reyes <40282876+jsebastianreyes@users.noreply.github.com>
2024-12-22 11:36:13 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
       if(observedDiv.current.offsetHeight !== height) {
 
         setHeight(observedDiv.current.offsetHeight);
-        window.parent.postMessage(height, "*");
+        ref.current.contentWindow.postMessage(height, origin);
       }
     });
   
